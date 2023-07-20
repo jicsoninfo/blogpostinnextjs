ee9356980e91a5f9d2546237d46a886ec8e04bc5
jicsoninfo <jicsoninfo@gmail.com>
2023-07-20 18:35:44 +0530

    create pages for routing

---
+++ src/app/blog/fordellayout.jsx
@@ -0,0 +1,12 @@
+
+// const Bloglayout =({children}) => {
+//     return (
+//       <div>
+//         <h1>Main layout</h1>
+//         {children}
+//         </div>
+//     )
+//   }
+  
+//   export default Bloglayout
+  
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+const Navbar =() => {
+    return (
+      <div>
+        {/* <h1>Navbar</h1> */}
+        <Link href="/">R B Sharma</Link>
+        </div>
+    )
+  }
+  
+  export default Navbar
+  
